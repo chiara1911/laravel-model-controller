f68278c01291af2c06b543afb57e6a17aac43509
chiara1911 <chiaracesari89@gmail.com>
2023-12-20 15:48:29 +0100

    add movie's cards

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,11 +5,36 @@
 
 @section('content')
 
-<ul>
-    @foreach ($movies as $movie )
-        <li>{{$movie->title}}</li>
-    @endforeach
-</ul>
 
-<h1>hello</h1>
+
+<h1>Movies</h1>
+<main>
+
+    <div class="container">
+
+        <h2 class="text-light">Movies</h2>
+        <div class="row">
+            {{-- @foreach ($movies as $key => $movie) --}}
+            @foreach ($movies as $movie )
+                <div class="col-12 col-md-3 col-lg-2">
+                    <div class="card-wrapper">
+                        <div class="img-card overflow-hidden">
+                            <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
+
+                        </div>
+
+                         <span
+                            class="p-2 d-flex flex-wrap align-content-center text-light text-uppercase">{{ $movie->title }}</span>
+
+                    </div>
+
+<span>{{$movie->title}}</span>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+
+
+</main>
 @endsection
